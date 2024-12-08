0153f9e00f0ee9d703d2f855626eef204a0eed09
EdwardChang0207 <thshguitar2015@gmail.com>
2024-12-08 11:39:25 +0800

    zj class code

--- ZeroJudge例題/a003-2.py
+++ ZeroJudge例題/a003-2.py
@@ -1,18 +1,1 @@
-#input:
-#line1: M D
-
-x = input().split() 
-M = int(x[0])
-D = int(x[1])
-
-#process:
-s = (M*2+D)%3
-#2. judge 吉 大吉 普通 by s
-if s == 0:
-    print("普通")
-elif s == 1:
-    print("吉")
-else:
-    print("大吉")
-
-#output:吉 大吉 普通
+a
